5bb11b3ab10d6a4ac210b711c8875c42f8fc5c7f
ieeah <lbnfnc90@gmail.com>
2021-10-26 15:08:10 +0200

    qualcosa non va nella ricerca nell' array

--- js/main.js
+++ js/main.js
@@ -12,16 +12,24 @@
         const loginBox = document.getElementById('login-box');
         // il campo input di tipo email da cui prelevare il valore
         const inputEmail = document.getElementById('mail');
+        // il div contenente il gioco vero e proprio
+        const gameBoard = document.getElementById('game');
+        // la schermata di errore al login
+        const denied = document.getElementById('denied');
+        // i pulsanti di chiusura
+        const closeButton = document.querySelectorAll('.close');
+        // il pulsante di generazione dei numeri
+        const genButton = document.getElementById('generate');
 
 
         // creazione dell'array contenente gli indirizzi mail autorizzati all'accesso
-        const mailList = ['info@boolean.com', 'paolo.duzioni@gmail.com', 'lbnfnc90@gmail.com', 'w.churchill@uk.gov.com', 'jesuswasahippie@hotmail.com'];
+        const mailList = ['info@boolean.com', 'paolo.duzioni@gmail.com', 'lbnfnc90@gmail.com', 'w.churchill@uk.gov.com', 'jesuswasahippie@hotmail.it'];
 
 
         //l'utente clicca sul pulsante di login
         btnLogin.addEventListener('click', function() {
-            //viene creata una nuova costante contenente la mail inserita nell input del form
-            const nowMail = inputEmail.value.trim().toLowerCase();
+            //viene creata una nuova variabile contenente la mail inserita nell input del form, con le dovute modifiche al formato
+            let nowMail = inputEmail.value.trim().toLowerCase();
 
             //parte la ricerca all'interno dell'array per verificare se sia autoriizzata o meno all'accesso
             //se nessuna mail è stata inserita, allora verrà stampato un alert
@@ -30,20 +38,18 @@
             } else {
                 // controllo presenza mail nella lista mailList
                 //altrimenti creiamo una flag
-                let foundMail = false;
+                // let foundMail = false;
                 for (i = 0; i <= 4 ; i++) {
                     //se la mail inserita è uguale ad una delle mail contenute nell'array
                     if (nowMail === mailList[i]) {
-                        //alla flag viene associato un valore true
-                        foundMail = true;
-                        //viene eseguito l'accesso
                         loginBox.classList.add('d-none');
-                        document.getElementById('game').classList.remove('d-none');
+                        denied.classList.remove('d-none');
                         break;
+                        
                     } else {
-                        foundMail = false;
+                        // foundMail = false;
                         loginBox.classList.add('d-none');
-                        document.getElementById('denied').classList.remove('d-none');
+                        gameBoard.classList.remove('d-none');
                         break;
 
                     }
@@ -59,10 +65,13 @@
         // stampare, all'interno di .dado #n1 e #n2 i due numeri generati
         // determinare quale dei due sia maggiore
         // stampare chi ha vinto
-        const genButton = document.getElementById('generate');
         genButton.addEventListener('click', function() {
-
         });
 
 
         // al click del div .close tutti i div != da loginBox prendono la classe d-none
+        closeButton.addEventListener('click', function() {
+            gameBoard.classList.add('d-none');
+            denied.classList.add('d-none');
+            loginBox.classList.remove('d-none');
+        });
